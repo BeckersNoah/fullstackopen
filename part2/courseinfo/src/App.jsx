802cf09dd701fceb339e8919fca3dca8e2868444
Noah Beckers <beckers.noah@gmail.com>
2025-03-30 18:06:13 +0300

    part 2

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,39 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = (props) => {
+  console.log(props.course.parts[0].name)
+  return (
+    <div>
+      <header>{props.course.name}</header>
+      <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
+      <p>{props.course.parts[1].name} {props.course.parts[1].exercises}</p>
+      <p>{props.course.parts[2].name} {props.course.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+export default App
